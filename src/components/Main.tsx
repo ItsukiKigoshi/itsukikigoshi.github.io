56f247a272dfc2d833a0a31db901449753fe0b4d
ItsukiKigoshi <itsukikigoshi@gmail.com>
2024-03-21 18:15:31 +0900

    Add new icons and update link buttons in Main component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,24 @@
 import GitHubIcon from "@mui/icons-material/GitHub";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import PodcastsIcon from "@mui/icons-material/Podcasts";
 import TwitterIcon from "@mui/icons-material/Twitter";
-import { Avatar, Box, IconButton, Stack, Typography } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Button,
+  IconButton,
+  Stack,
+  Typography,
+} from "@mui/material";
 
 export default function Main() {
   const links = [
+    {
+      name: "GitHub",
+      Icon: GitHubIcon,
+      href: "https://www.github.com/itsukikigoshi",
+    },
     {
       name: "Instagram",
       Icon: InstagramIcon,
@@ -17,14 +30,9 @@ export default function Main() {
       href: "https://www.twitter.com/itsukikigoshi",
     },
     {
-      name: "GitHub",
-      Icon: GitHubIcon,
-      href: "https://www.github.com/itsukikigoshi",
-    },
-    {
-      name: "LinkedIn",
-      Icon: LinkedInIcon,
-      href: "https://www.linkedin.com/in/itsukikigoshi",
+      name: "Podcast",
+      Icon: PodcastsIcon,
+      href: "https://podcasters.spotify.com/pod/show/n-dimensions",
     },
   ];
 
@@ -43,6 +51,50 @@ export default function Main() {
       </IconButton>
     )
   );
+
+  // const works = [
+  //   {
+  //     title: "ICU Catalogue",
+  //     description: "国際基督教大学の履修登録アプリ",
+  //     type: "Personal Project",
+  //     role: "Frontend Developer",
+  //     image: "/icu-catalogue.png",
+  //     url: "https://www.catalogue.icu",
+  //   },
+  //   {
+  //     title: "Seiryofes.com (2023)",
+  //     description: "日比谷高校文化祭の整理券アプリ",
+  //     type: "Team Project",
+  //     role: "Frontend Developer",
+  //     image: "/seiryofes.jpeg",
+  //     url: "https://2023.seiryofes.com",
+  //   },
+  // ];
+
+  // const worksCards = works.map((work) => (
+  //   <Card
+  //     sx={{ maxWidth: 345 }}
+  //     key={work.title}
+  //     color="primary"
+  //     component="a"
+  //     href={work.url}
+  //     target="_blank"
+  //   >
+  //     <CardMedia sx={{ height: 140 }} image={work.image} title={work.title} />
+  //     <CardContent>
+  //       <Typography gutterBottom variant="h5" component="div">
+  //         {work.title}
+  //       </Typography>
+  //       <Typography variant="body2" color="text.secondary">
+  //         {work.role}
+  //       </Typography>
+  //       <Typography variant="body2" color="text.secondary">
+  //         {work.description}
+  //       </Typography>
+  //     </CardContent>
+  //   </Card>
+  // ));
+
   return (
     <Box
       sx={{
@@ -50,16 +102,41 @@ export default function Main() {
         placeItems: "center", // Center the content horizontally and vertically
       }}
     >
-      <Avatar
-        alt={`Itsuki Kigoshi's Profile Picture`}
-        src={`/profile.jpg`}
-        sx={{ width: 200, height: 200 }}
-      />
-      <Typography sx={{ fontSize: 28, mt: 2 }}>Itsuki KIGOSHI</Typography>
-      <Typography sx={{ m: 2 }}>{`ICU '27 (2004)`}</Typography>
-      <Stack direction="row" spacing={2} alignItems="flex-end">
-        {linkButtons}
+      <Stack
+        spacing={1}
+        sx={{
+          display: "grid",
+          placeItems: "center", // Center the content horizontally and vertically
+        }}
+        my={2}
+      >
+        <Avatar
+          alt={`Itsuki Kigoshi's Profile Picture`}
+          src={`/profile.jpg`}
+          sx={{ width: 200, height: 200 }}
+        />
+        <Typography sx={{ fontSize: 28 }}>Itsuki KIGOSHI</Typography>
+        <Typography>{`ICU '27 (2004)`}</Typography>
       </Stack>
+      <Stack useFlexGap spacing={1}>
+        <Button
+          startIcon={<LinkedInIcon />}
+          variant="contained"
+          href="https://www.linkedin.com/in/itsukikigoshi"
+          target="_blank"
+        >
+          Open to Work!
+        </Button>
+        <Stack direction="row" spacing={2} alignItems="flex-end">
+          {linkButtons}
+        </Stack>
+      </Stack>
+      {/* <Stack spacing={1}>
+        <Typography variant="h5">Works</Typography>
+        <Stack direction="row" spacing={2}>
+          {worksCards}
+        </Stack>
+      </Stack> */}
     </Box>
   );
 }
