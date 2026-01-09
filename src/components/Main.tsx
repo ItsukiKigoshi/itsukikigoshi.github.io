30c3021919d501db63606b61bbf36a068d105610
ItsukiKigoshi <itsukikigoshi@gmail.com>
2026-01-09 05:07:04 -0500

    Feat: Change Math to Earth Science in tagline

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -84,7 +84,7 @@ export default function Main() {
                 <Typography sx={{fontSize: 28}}>Itsuki Kigoshi / 木越 斎</Typography>
                 <Typography>ICU&apos;27 (2004)</Typography>
                 <Typography>
-                    Climbing, Contemporary Art, Earth Science
+                    Earth Science, Climbing, Contemporary Art
                 </Typography>
             </Stack>
             <Stack sx={{justifyContent: "center"}} direction="row" spacing={2}>
